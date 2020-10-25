1a9ab3f52f127dfc077698d6cbe6e6c16ddb3899
sunny <jjohye11@gmail.com>
2020-10-25 17:04:39 +0900

    2048 실습내용

---
+++ game2048/sunny-game2048/src/component/AboveGame.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function AboveGame() {
+  return (
+    <div className="above-game">
+      <p className="game-intro"><strong>2048 tile!</strong>_조혜선실습</p>
+      <a href="/" className="restart-button">New Game</a> 
+      {/* 위 코드의 의미가 궁금 */}
+    </div>
+  );
+}
+
---
+++ game2048/sunny-game2048/src/component/Game.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import times from 'lodash/times';
+import { MAX_POS } from '../constant';
+import { getInitialTileList } from '../util/tile';
+import useMoveTile from '../hook/useMoveTile';
+import Tile from './Tile';
+
+export default function Game({setScore}) {
+  const [tileList, setTileList] = useState(getInitialTileList);
+  // up, down, left, right 로 타일을 옮기는게 필요
+  useMoveTile({tileList, setTileList, setScore});
+  return (
+    <div className="game-container">
+      <div className="grid-container">
+        {
+          times(MAX_POS, index => (
+          <div key={index} className="grid-row">
+            {times(MAX_POS, index2 => (
+            <div key={index2} className="grid-cell"></div>
+            ))}
+          </div>
+          ))
+        }
+      </div>
+
+      <div className="tile-container">
+        {tileList.map(item => (
+          <Tile {...item} />
+        ))}
+      </div>
+    </div>
+  );
+}
